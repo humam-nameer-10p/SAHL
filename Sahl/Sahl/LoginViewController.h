16077131f683357e19292bc4fe8b708ab9679e0e
Humam <humam.nameer@tenpearls.com>
2015-11-12 20:07:36 +0500

    initial commit

---
+++ Sahl/Sahl/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Sahl
+//
+//  Created by Ovais Bawany on 11/12/15.
+//  Copyright © 2015 Humam Nameer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ Sahl/Sahl/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Sahl
+//
+//  Created by Ovais Bawany on 11/12/15.
+//  Copyright © 2015 Humam Nameer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
